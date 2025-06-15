c66e615117fe4df4e396dfc06eb4328f987eccab
Delete Gir <delete@lidowin.com>
2025-06-15 12:27:35 +0700

    refactor: move controller and service to each module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { UserModule } from './user/user.module';
-import { BarberModule } from './barber/barber.module';
-import { BarberController } from './barber/barber.controller';
-import { BarberService } from './barber/barber.service';
 import { BookingModule } from './booking/booking.module';
-import { BookingController } from './booking/booking.controller';
-import { BookingService } from './booking/booking.service';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BarberModule } from './barber/barber.module';
 
 @Module({
-  imports: [UserModule, BarberModule, BookingModule],
-  controllers: [
-    AppController,
-    UserController,
-    BarberController,
-    BookingController,
-  ],
-  providers: [
-    AppService,
-    PrismaService,
-    UserService,
-    BarberService,
-    BookingService,
-  ],
+  imports: [BookingModule, PrismaModule, UserModule, BarberModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
