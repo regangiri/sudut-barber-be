162f1a0ae0628debc0b7735356c48028d426f905
Delete Gir <delete@lidowin.com>
2025-07-02 14:41:44 +0700

    chore: test workflow

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'localhost:3000', // atau frontend URL-mu
   });
+  //test workflow
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
